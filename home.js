265645adfd51d9f7a3584205631f9ccd51d10aa8
democapg123 <120020392+democapg123@users.noreply.github.com>
2022-12-15 18:40:36 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,8 +1,3 @@
 <script type="text/javascript">
-    (function (i, s, o, g, r, a, m) {
-        i[r] = i[r], a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://static.critizr.com/widgets/<API_KEY>.js', 'cz');
+console.log('critizr')
 </script>
