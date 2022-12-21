d53e1ef28122d2bcbed467807c8e707c20cff5b3
democapg123 <120020392+democapg123@users.noreply.github.com>
2022-12-21 16:16:57 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,20 @@
-<script >
-var a='critizr';
-return a;
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://tagging.example.com/gtag/js?id=G-60FDXKEXRK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-60FDXKEXRK', {
+     transport_url: 'https://sgtm-ahava-tagging-server.azurewebsites.net',
+     first_party_collection: true,
+     'send_page_view': false,
+  });
+
+<script type="text/javascript"> (function(i,s,o,g,r,a,m) 
+ {i[r]=i[r],a=s.createElement(o), m=s.getElementsByTagName(o)[0];
+ a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://static.critizr.com/widgets/preprod/API_KEY.js','cz'); 
+</script>
+
 </script>
