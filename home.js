92dad4f28c0d75e2d19e43d2fd5d9a9a155b5af3
democapg123 <120020392+democapg123@users.noreply.github.com>
2022-12-21 16:39:40 +0530

    Update home.js

--- home.js
+++ home.js
@@ -11,10 +11,10 @@
      first_party_collection: true,
      'send_page_view': false,
   });
-
+</script>
 <script type="text/javascript"> (function(i,s,o,g,r,a,m) 
  {i[r]=i[r],a=s.createElement(o), m=s.getElementsByTagName(o)[0];
  a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://static.critizr.com/widgets/preprod/API_KEY.js','cz'); 
 </script>
 
-</script>
+
