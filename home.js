e9705bd2a90f68d94b1b5fef19b25810c658207a
pooborhade <119406443+pooborhade@users.noreply.github.com>
2022-12-23 15:24:14 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,18 +1,7 @@
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://tagging.example.com/gtag/js?id=G-60FDXKEXRK"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  gtag('event', 'add_to_cart', {
+  'page_path': '/sampleEcommTest'
 
-  gtag('config', 'G-60FDXKEXRK', {
-     transport_url: 'https://sgtm-ahava-tagging-server.azurewebsites.net',
-     first_party_collection: true,
-     'send_page_view': true,
-  });
-gtag('event', 'page_view', {
-  send_to: 'G-60FDXKEXRK'
-});
+})
 </script>
 
