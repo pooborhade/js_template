ca246a0ad824d9fae7924dd4eb2372ddbc9a82e1
democapg123 <120020392+democapg123@users.noreply.github.com>
2022-12-27 14:28:00 +0530

    Create new_tag.js

---
+++ new_tag.js
@@ -0,0 +1,13 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=TAG_ID&l=myNewName"></script>
+<script>
+  window.myNewName = window.myNewName || [];
+  function gtag(){myNewName.push(arguments);}
+  gtag('js', new Date());
+
+    gtag('config', 'G-60FDXKEXRK', {
+     transport_url: 'https://sgtm-ahava-tagging-server.azurewebsites.net',
+     first_party_collection: true,
+     'send_page_view': true,
+  });
+</script>  
